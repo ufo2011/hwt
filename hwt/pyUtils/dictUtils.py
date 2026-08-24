636207ed3f076f18ad5d05c8efa31bfecdd74641
Nic30 <nic30@seznam.cz>
2026-08-24 20:17:53 +0200

    feat(pyutils.dictUtils): dictMerge add recursive mode

--- hwt/pyUtils/dictUtils.py
+++ hwt/pyUtils/dictUtils.py
@@ -4,9 +4,28 @@ KeyT = TypeVar("KeyT")
 ValueT = TypeVar("ValueT")
 
 
-def dictMerge(*dicts: tuple[dict[KeyT, ValueT], ...]) -> dict[KeyT, ValueT]:
+def dictMerge(*dicts: tuple[dict[KeyT, ValueT], ...], recursively=False) -> dict[KeyT, ValueT]:
+    """
+    :note: input diects are unmodified and the returned dict is a new instance
+        but key and value objects are the original
+        (recursively may create new instance of value in order to avoid modification of original)
+    """
     d: dict[KeyT, ValueT] = {}
+    isFirstDict = True
     for dIn in dicts:
-        d.update(dIn)
+        if recursively and not isFirstDict:
+            for k, v in dIn.items():
+                if isinstance(v, dict):
+                    existing = d.get(k, None)
+                    if existing is not None and isinstance(existing, dict):
+                        d[k] = dictMerge(existing, v)
+                        continue
+                d[k] = v
+            
+        else:
+            d.update(dIn)
+
+        isFirstDict = False
+
     return d
     
